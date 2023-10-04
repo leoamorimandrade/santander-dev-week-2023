94c625b2ebd2e8ac3c4d330f8fed29f6bab20cc3
leoamorimandrade <142458153+leoamorimandrade@users.noreply.github.com>
2023-10-03 22:57:15 -0300

    Bugfix CORS

--- src/main/java/me/dio/santanderdevweek2023/SantanderDevWeek2023Application.java
+++ src/main/java/me/dio/santanderdevweek2023/SantanderDevWeek2023Application.java
@@ -8,7 +8,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Correção bugfix CORS
 @OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class SantanderDevWeek2023Application {
